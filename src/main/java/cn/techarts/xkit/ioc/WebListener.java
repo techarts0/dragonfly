121dea4f16ed477c9d5fd3a9e0642c6f615754b7
rocwon <pengfei@techarts.com.cn>
2024-09-16 09:13:57 +0800

    Added a WebListener to IOC context

---
+++ src/main/java/cn/techarts/xkit/ioc/WebListener.java
@@ -0,0 +1,54 @@
+package cn.techarts.xkit.ioc;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import cn.techarts.xkit.util.Hotpot;
+
+public class WebListener implements ServletContextListener {
+		
+	@Override
+	public void contextInitialized(ServletContextEvent arg) {
+		var context = arg.getServletContext();
+		var classpath = this.getRootClassPath();
+		var xmlOrJson = getCraftConfiguration();
+		var config = classpath.concat("config.properties");
+		var configs = Hotpot.resolveProperties(config);
+		Context.make(classpath, xmlOrJson, configs).cache(context);
+	}
+	
+	private String getRootClassPath() {
+		var result = getClass().getResource("/");
+		if(result == null || result.getPath() == null) {
+			throw new Panic("Failed to get resource path.");
+		}
+		return result.getPath();
+	}
+	
+	private String getCraftConfiguration() {
+		var res = getClass().getResource("/beans.xml");
+		if(res == null || res.getPath() == null) {
+			res = getClass().getResource("/beans.json");
+		}
+		if(res == null || res.getPath() == null) {
+			throw new Panic("Failed to get resource path.");
+		}
+		return res.getPath();
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		var ctx = arg0.getServletContext();
+		var context = Context.from(ctx);
+		if(context != null) context.close();
+	}
+	
+	public<T> T get(ServletContext ctx, String id, Class<T> clzz)
+	{
+		return Context.from(ctx).get(id, clzz);
+	}
+	
+	public Object get(ServletContext ctx, String id){
+		return Context.from(ctx).get(id);
+	}
+}
