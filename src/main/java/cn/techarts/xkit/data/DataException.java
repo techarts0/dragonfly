550b4418337a7779cd83fd333f74480c77bc5093
techarts <pengfei@techarts.com.cn>
2024-10-15 01:00:19 +0800

    Nothing

--- src/main/java/cn/techarts/xkit/data/DataException.java
+++ src/main/java/cn/techarts/xkit/data/DataException.java
@@ -19,24 +19,20 @@ package cn.techarts.xkit.data;
 /**
  * @author rocwon@gmail.com
  */
-public class DataException extends RuntimeException
-{	
+public class DataException extends RuntimeException{	
 	
 	private static final long serialVersionUID = 1L;
 
-	public DataException( String cause)
-	{
+	public DataException( String cause){
 		super( cause);
 		
 	}
 	
-	public DataException( String cause, Throwable e)
-	{
+	public DataException( String cause, Throwable e){
 		super( cause, e);
 	}
 	
-	public DataException( int errno, String cause, Throwable e)
-	{
+	public DataException( int errno, String cause, Throwable e){
 		super( cause, e);
 	}
 }
