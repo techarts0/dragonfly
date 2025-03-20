e8252771b641cccb6593a96f5ed069fc544d7c66
techarts <pengfei@techarts.com.cn>
2025-03-20 12:34:59 +0800

    Updated the license declaration.

--- src/main/java/cn/techarts/dragonfly/web/jsonrpc/JsonRpcError.java
+++ src/main/java/cn/techarts/dragonfly/web/jsonrpc/JsonRpcError.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright (C) 2024 techarts.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.techarts.dragonfly.web.jsonrpc;
 
+/**
+ * @author rocwon@gmail.com
+ */
 public class JsonRpcError extends JsonRpcMessage{
 	private Error error;
 	public static final int PARSE_ERROR = -32700;
